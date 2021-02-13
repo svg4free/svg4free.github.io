66bc22e95190242f236f90935bb560966321054d
svg4free <55647799+svg4free@users.noreply.github.com>
2021-02-13 10:20:08 -0600

    Add files via upload

---
+++ ContentLoader.js
@@ -0,0 +1,21 @@
+const xhr = new XMLHttpRequest;
+url = window.location.hash.split(#)[1];
+history.replaceState(null, "", "/" + url);
+
+function loadArticle(){
+	if(xhr.readyState === XMLHttpRequest.DONE) {
+    var status = xhr.status;
+    if (status === 0 || (status >= 200 && status < 400)) {
+      // The request has been completed successfully
+      document.getElementById("column2").innerHTML = xhr.responseXML.getElementById("mainContent").innerHTML);
+    } else {
+      // Oh no! There has been an error with the request!
+	  alert("Article has not been loaded");
+    }
+  }
+}
+
+xhr.open("GET", window.location.hostname + "/" + url + ".article", true);
+xhr.onreadystatechange = loadArticle;
+xhr.responseType = "document";
+xhr.send();
