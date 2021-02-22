a4c430f59699d140dc348f789cca2b960065fbad
svg4free <55647799+svg4free@users.noreply.github.com>
2021-02-22 13:24:43 -0600

    Add files via upload

--- ContentLoader.js
+++ ContentLoader.js
@@ -1,26 +1,37 @@
-  
 //This script only runs on the layout page
 //Check to see if there is a hash in the url. eg. https://svg4free.github.io/Layout#hash
 
 url = window.location.hash.split("#")[1];
 
 function loadArticle(){
-	if(xhr.readyState === XMLHttpRequest.DONE) {
+	
+if(xhr.readyState === XMLHttpRequest.DONE) {
     var status = xhr.status;
     if (status === 0 || (status >= 200 && status < 400)) {
 		// The request has been completed successfully
 		//Add the contenet to column 2 of Layout.html
+		
 		document.getElementById("column2").appendChild(xhr.responseXML.getElementById("mainContent"));
+		var articleTitle = xhr.responseXML.getElementsByTagName("title");
 		var links = xhr.responseXML.getElementsByTagName("link");
 		var scripts = xhr.responseXML.getElementsByTagName("script");
-		for (i = 0; i < links.length; i++){
-			links[i].href = links[i].href;
-			document.head.appendChild(links[i]);
+		
+		if(articleTitle.length){
+			document.getElementsByTagName("title")[0].textContent = articleTitle[0].textContent;
+		}
+		
+		if (links.length){
+			for (i = 0; i < links.length; i++){
+				links[i].href = links[i].href;
+				document.head.appendChild(links[i]);
+			}
 		}
 	
-		for (i = 0; i < scripts.length; i++){
-			scripts[i].src = scripts[i].src;
-			document.head.appendChild(scripts[i]);
+		if (scripts.length){
+			for (i = 0; i < scripts.length; i++){
+				scripts[i].src = scripts[i].src;
+				document.head.appendChild(scripts[i]);
+			}
 		}
     } else {
 		// Oh no! There has been an error with the request!
