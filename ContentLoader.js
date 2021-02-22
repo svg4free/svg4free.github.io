4fcc065a6c3ca07d6c4fef85fe824a2173be6785
svg4free <55647799+svg4free@users.noreply.github.com>
2021-02-22 16:06:58 -0600

    Add files via upload

--- ContentLoader.js
+++ ContentLoader.js
@@ -11,29 +11,40 @@ if(xhr.readyState === XMLHttpRequest.DONE) {
 		// The request has been completed successfully
 		//Add the contenet to column 2 of Layout.html
 		
-		document.getElementById("column2").appendChild(xhr.responseXML.getElementById("mainContent"));
+		//create variables for title, link and script tags in loaded content
 		var articleTitle = xhr.responseXML.getElementsByTagName("title");
 		var links = xhr.responseXML.getElementsByTagName("link");
 		var scripts = xhr.responseXML.getElementsByTagName("script");
 		
+		//if there is at least one title tag
 		if(articleTitle.length){
 			document.getElementsByTagName("title")[0].textContent = articleTitle[0].textContent;
 		}
 		
+		//if there is at least one link tag
 		if (links.length){
 			for (i = 0; i < links.length; i++){
+				//this is needed to convert relative paths to absolute paths
 				links[i].href = links[i].href;
+				//add the link tags to Layout.html
 				document.head.appendChild(links[i]);
 			}
 		}
-	
+		
+		//if there is at least one script tag
 		if (scripts.length){
 			for (i = 0; i < scripts.length; i++){
+				//for every script tag create a new script element
 				var _script = document.createElement("script");
+				//set its source to equal to the included src attribute
 				_script.setAttribute("src", scripts[i].src);
+				//append and run the scripts
 				document.body.appendChild(_script);
 			}
 		}
+		//load the article content
+		document.getElementById("column2").appendChild(xhr.responseXML.getElementById("mainContent"));
+		
     } else {
 		// Oh no! There has been an error with the request!
 		alert("Article has not been loaded");
