9b0b703ff631e1fb92fac77a0480202ebdc583d7
svg4free <55647799+svg4free@users.noreply.github.com>
2021-02-22 10:21:12 -0600

    Add files via upload

--- ContentLoader.js
+++ ContentLoader.js
@@ -3,17 +3,21 @@
 //Check to see if there is a hash in the url. eg. https://svg4free.github.io/Layout#hash
 
 url = window.location.hash.split("#")[1];
+var links;
+var scripts;
 
 function loadArticle(){
 	if(xhr.readyState === XMLHttpRequest.DONE) {
     var status = xhr.status;
     if (status === 0 || (status >= 200 && status < 400)) {
-	// The request has been completed successfully
-	//Add the contenet to column 2 of Layout.html
-	document.getElementById("column2").appendChild(xhr.responseXML.getElementById("mainContent"));
+		// The request has been completed successfully
+		//Add the contenet to column 2 of Layout.html
+		document.getElementById("column2").appendChild(xhr.responseXML.getElementById("mainContent"));
+		links = xhr.responseXML.getElementsByTagName("link");
+		scripts = xhr.responseXML.getElementsByTagName("script");
     } else {
-      // Oh no! There has been an error with the request!
-	  alert("Article has not been loaded");
+		// Oh no! There has been an error with the request!
+		alert("Article has not been loaded");
     }
   }
 }
@@ -24,12 +28,10 @@ function loadResources(){
 	//ss.href = window.location.origin + "/" + url + ".css";
 	//document.head.appendChild(ss);
 	
-	var links = xhr.responseXML.getElementsByTagName("link");
 	for (i = 0; i < links.length; i++){
 		document.head.appendChild(links[i]);
 	}
 	
-	var scripts = xhr.responseXML.getElementsByTagName("script");
 	for (i = 0; i < scripts.length; i++){
 		document.head.appendChild(scripts[i]);
 	}
